bb32ff5b35600b1d7c9ccdd19cc2a56e3e3ffc2d
Michał Oręziak <micorix.dev@gmail.com>
2023-02-26 11:03:21 +0100

    Fix event names in lib/gcal_mapper.py

--- lib/gcal_mapper.py
+++ lib/gcal_mapper.py
@@ -1,3 +1,5 @@
+import re
+
 from lib.datatypes import ScheduleEventInfo
 from lib.settings import settings
 
@@ -8,24 +10,16 @@ def stringify_datetime(date_str, h_str):
 
 def map_schedule_event_to_gcal_event(schedule_event: ScheduleEventInfo):
     print(schedule_event)
-    type = schedule_event.get('event_type')
-    if schedule_event.get('event_type') == "(w)":
-        type = "Wykład"
-    elif schedule_event.get('event_type') == "(ć)":
-        type = "Ćwiczenia"
-    elif schedule_event.get('event_type') == "(L)":
-        type = "Labolatoria"
     name = schedule_event.get("name")
+    match = re.match(r"^(.*) - \((.*)\)", name)  # <subject name> - (<event type>)
+
+    name = match.group(1)
+    event_type = match.group(2)
 
     return {
-        "summary": name.strip(
-            " - (Wykład)").strip(" - (Ćwiczenia)").strip(" - (Labolatoria)"),
-        "location": settings.university_location + ' at ' + schedule_event.get('location'),
-        "description": "\n".join(
-            [
-                f"Type: {type}",
-            ]
-        ),
+        "summary": name,
+        "location": f"{settings.university_location} at {schedule_event.get('location')}",
+        "description": f"Type: {event_type}",
         "start": {
             "dateTime": stringify_datetime(
                 schedule_event.get("date"), schedule_event.get("time").get("start")
